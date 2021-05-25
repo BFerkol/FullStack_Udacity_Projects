e118be6d585b2e017942c0f108056cb7f2b8e614
benferkol <b.ferkol@gmail.com>
2021-05-25 13:54:19 -0700

    finished delete_drink implementation

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -21,12 +21,14 @@ def get_drinks():
     try:
         # Query all drinks
         drinks = Drink.query.all()
+
         # Return JSON success object (success -> true)
         # and (for statement interating through list of all drinks using drink.short)
         return jsonify({
             'success': True,
             'drinks': [drink.short() for drink in drinks]
-        })
+        }), 200
+
     # If fails, throw a 500 (internal error) error code
     except:
         abort(500)
@@ -38,12 +40,14 @@ def get_drinks_detail(jwt_token):
     try:
         # Query all drinks
         drinks = Drink.query.all()
+
         # Return JSON success object (success -> true)
         # and (for statement iterating through list of all drinks using drink.long)
         return jsonify({
             'success': True,
             'drinks': [drink.long() for drink in drinks]
-        })
+        }), 200
+
         # If fails, throw a 500 (internal error) error code
     except:
         abort(500)
@@ -62,11 +66,11 @@ def post_drink(jwt_token):
         new_drink.insert()
         
         # Return JSON success object (success -> true)
-        # and the new drink using drink.long representation
+        # and the new drink using drink.long
         return jsonify({
             'success': True,
             'drinks': new_drink.long()
-        })
+        }), 200
 
     # If drink creation fails, throw a 422 (unprocessable) error code
     except Exception:
@@ -94,31 +98,29 @@ def patch_drink(jwt_token, id):
         abort(404)
 
     try:
-        # Query the drink with the match id number
+        # Query the drink with the matching id number
         update_drink = Drink.query.filter_by(Drink.id=id).one_or_none()
 
         # If the query returns nothing, throw abort 404 (not found) errow code
         if not update_drink:
             abort(404)
 
-        # Update the drink details and update to the database
+        # Update the drink details
         update_drink.title = data.get('title')
         update_drink.recipe = data.get('recipe')
         update_drink.update()
 
+        # Return JSON success object (success -> true)
+        # and the updated drink using drink.long
         return jsonify({
-            'success': True
+            'success': True,
             'drinks': update_drink.long()
-        })
+        }), 200
 
-    #  If drink update fails, throw a 422 (unprocessable) error code
+    # If drink update fails, throw a 422 (unprocessable) error code
     except:
         abort(422)
 
-
-
-
-        
 
 '''
 @TODO implement endpoint
@@ -132,7 +134,28 @@ def patch_drink(jwt_token, id):
 '''
 @app.route('/drinks/<id>', method=['DELETE'])
 @requires_auth('delete:drinks')
-def delete_drink(jwt_token, id)
+def delete_drink(jwt_token, id):
+    try:
+        # Query the drink with the matching id number
+        remove_drink = Drink.query.filter_by(Drink.id=id).one_or_none()
+        
+        # If the query returns nothing, throw abort 404 (not found) errow code
+        if not remove_drink:
+            abort(404)
+
+        # Delete the drink
+        remove_drink.delete()
+
+        # Return JSON success object (success -> true)
+        # and the deleted drink's id number
+        return jsonify({
+            'success': True,
+            'delete': id
+        }), 200
+
+    # If drink update fails, throw a 422 (unprocessable) error code
+    except:
+        abort(422)
 
 
 # Error Handling
