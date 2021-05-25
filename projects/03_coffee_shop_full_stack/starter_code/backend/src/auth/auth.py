bf761b95eca60082faffc301285ed6132a55bf9f
benferkol <b.ferkol@gmail.com>
2021-05-24 23:51:37 -0700

    deleted TODO comments in backend/auth.py

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -59,18 +59,8 @@ def get_token_auth_header():
     # Return the token part of the header
     return header_sections[1]
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
 
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
-# Taken from course example code
+# Taken from coursework code
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
         raise AuthError({
@@ -84,19 +74,7 @@ def check_permissions(permission, payload):
         }, 403)
     return True
 
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
 
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
 def verify_decode_jwt(token):
     # Get the public key from the URL/AUTH0
     json_url = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
