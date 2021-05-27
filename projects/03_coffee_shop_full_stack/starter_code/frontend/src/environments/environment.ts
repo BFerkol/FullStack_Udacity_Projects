09600fb334ec2db65813afe158d128cfbb7006ca
benferkol <b.ferkol@gmail.com>
2021-05-27 14:18:51 -0700

    had http instead of https in environment.ts file

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: 'https://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'fsnd-cfikido3.us.auth0.com', // the auth0 domain prefix
+    url: 'fsnd-cfikido3.us', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'xigFGiZrGW5vn0GG8AMsinZsDJHW2Tf6', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    callbackURL: 'https://localhost:8100/login-results', // the base url of the running ionic application. 
   }
 };
